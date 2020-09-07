fa14c49b7ad4a3594c8d3f7a93cca5ec4721e30b
Mathieu St-Vincent <m@mats.dev>
2020-09-07 16:21:49 -0400

    WIP: Store - missing get_multi lifetime problem & set with multiple ids

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,1 @@
-pub struct HAStore {}
-
-impl HAStore {
-  fn get() -> String {
-    return String::from("Got data");
-  }
-}
-
-#[cfg(test)]
-mod tests {
-  use crate::HAStore;
-
-  #[test]
-  fn it_works() {
-    assert_eq!(HAStore::get(), "Got data");
-  }
-}
+mod store;
---
+++ src/store.rs
@@ -0,0 +1,67 @@
+extern crate lru;
+
+use lru::{ LruCache };
+use std::hash::Hash;
+
+pub struct Store<K, V> {
+  cache: LruCache<K, V>
+}
+
+impl<'a, K: Clone + Hash + Eq, V> Store<K, V> {
+  pub fn new() -> Self {
+    Store {
+      cache: LruCache::new(1000),
+    }
+  }
+
+  pub fn get(&mut self, key: &K) -> Option<&V> {
+    return self.cache.get(key);
+  }
+
+  pub fn get_multi(&mut self, keys: &Vec<&K>) -> Vec<Option<&V>> {
+    return keys
+      .iter()
+      .map(|&key| self.get(key))
+      .collect::<Vec<Option<&V>>>();
+  }
+
+  // TODO: Set multiple records
+  // recordKey, keys, values
+  pub fn set(&mut self, key: K, value: V) -> Option<V> {
+    return self.cache.put(key, value);
+   }
+
+  pub fn clear(&mut self) {
+    self.cache.clear();
+  }
+
+  pub fn size(&mut self) -> usize {
+    return self.cache.len();
+  }
+}
+
+#[cfg(test)]
+mod tests {
+  use super::Store;
+
+  #[test]
+  fn it_works() {
+    let mut store = Store::new();
+
+    store.set("apple", 3);
+    store.set("banana", 2);
+    assert_eq!(store.get(&"apple"), Some(&3));
+    assert_eq!(store.get(&"banana"), Some(&2));
+    assert!(store.get(&"pear").is_none());
+    assert_eq!(store.set("banana", 4), Some(2));
+    assert_eq!(store.set("pear", 5), None);
+    assert_eq!(store.get(&"pear"), Some(&5));
+    assert_eq!(store.get(&"banana"), Some(&4));
+    assert_eq!(store.size(), 3);
+
+    assert_eq!(store.get_multi(&vec![&"pear", &"banana"]), [Some(&5), Some(&4)]);
+
+    store.clear();
+    assert_eq!(store.size(), 0);
+  }
+}
